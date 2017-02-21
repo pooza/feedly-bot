33cbf878e19f65d0c14371068f685aa9ab269ea6
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2017-02-21 11:02:13 +0900

    refs #320

--- feedly_deliverer.rb
+++ feedly_deliverer.rb
@@ -11,6 +11,18 @@ require 'feedly_deliverer/config'
 require 'feedly_deliverer/mail_deliverer'
 
 config = FeedlyDeliverer::Config.new
+config['days'] = 7
 config.parse!
 
-p config
+if (config['access_token']['expires_on'] - config['days']) < Date.today
+  deliverer = FeedlyDeliverer::MailDeliverer.new
+  deliverer.subject = 'アクセストークンの期限'
+  deliverer.to = config['mail_to']
+  deliverer.body = [
+    'アクセストークンの期限は、',
+    config['access_token']['expires_on'].to_s,
+    'です。更新しましょう。',
+    '<https://feedly.com/v3/auth/dev>',
+  ].join
+  deliverer.deliver!
+end
