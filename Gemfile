a79a55c1fcf01310d04abe9a0d11be4e399b5c09
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2017-02-21 12:12:14 +0900

    fixes #320

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 gem 'mail'
-gem 'feedly'
+gem 'feedlr'
 
--- feedly_deliverer.rb
+++ feedly_deliverer.rb
@@ -9,6 +9,8 @@ $LOAD_PATH.push(File.join(ROOT_DIR, 'lib'))
 
 require 'feedly_deliverer/config'
 require 'feedly_deliverer/mail_deliverer'
+require 'feedlr'
+require 'syslog/logger'
 
 config = FeedlyDeliverer::Config.new
 config['days'] = 7
@@ -24,5 +26,50 @@ if (config['access_token']['expires_on'] - config['days']) < Date.today
     'です。更新しましょう。',
     '<https://feedly.com/v3/auth/dev>',
   ].join
+  deliverer.deliver!
+  exit
+end
+
+feedlr = Feedlr::Client.new({
+  oauth_access_token: config['access_token']['token'],
+  sandbox: false,
+  logger: Syslog::Logger.new('feedlr'),
+})
+
+entries = []
+begin
+  category_id = "user/#{feedlr.user_profile.id}/category/global.all"
+  entry_ids = feedlr.stream_entries_ids(category_id, count:100, unreadOnly:true)['ids']
+  feedlr.user_entries(entry_ids).each do |entry|
+    entries.push({
+      origin: entry['origin']['title'],
+      title: entry['title'],
+      published: Time.at((entry['published'].to_i / 1000).ceil),
+      url: entry['alternate'].first['href'],
+    })
+  end
+rescue => e
+  deliverer = FeedlyDeliverer::MailDeliverer.new
+  deliverer.subject = e.class
+  deliverer.to = config['mail_to']
+  deliverer.body = e.message
+  deliverer.deliver!
+  exit
+end
+
+unless entries.empty?
+  body = []
+  entries.each do |entry|
+    body.push("サイト: #{entry[:origin]}")
+    body.push("タイトル: #{entry[:title]}")
+    body.push("日付: #{entry[:published].strftime('%F %T')}")
+    body.push("URL: <#{entry[:url]}>")
+    body.push(nil)
+  end
+
+  deliverer = FeedlyDeliverer::MailDeliverer.new
+  deliverer.subject = "新着記事 #{Time.now.strftime('%F %T')}"
+  deliverer.to = config['mail_to']
+  deliverer.body = body.join("\n")
   deliverer.deliver!
 end
