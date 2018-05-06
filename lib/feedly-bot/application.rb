fc02e8cd9b1a321e8374dd9a093dd40fd7871f35
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-05-06 16:07:44 +0900

    #9

--- lib/feedly-bot/application.rb
+++ lib/feedly-bot/application.rb
@@ -1,18 +1,17 @@
 require 'feedly-bot/feedly'
 require 'feedly-bot/slack'
-require 'feedly-bot/package'
 require 'feedly-bot/logger'
 
 module FeedlyBot
   class Application
     def initialize
       @slack = Slack.new
-      @logger = Logger.new(Package.name)
+      @logger = Logger.new
       @feedly = Feedly.new
     end
 
     def execute
-      @logger.info({message: 'start', version: Package.version})
+      @logger.info({message: 'start'})
       if @feedly.outdated?
         message = {expires_on: @feedly.expires_on, url: @feedly.auth_url}
         @slack.say(message)
@@ -22,9 +21,9 @@ module FeedlyBot
         @slack.say(entry)
         @logger.info(entry)
       end
-      @logger.info({message: 'complete', version: Package.version})
+      @logger.info({message: 'complete'})
     rescue => e
-      message = {class: e.class, message: e.message, version: Package.version}
+      message = {class: e.class, message: e.message}
       @slack.say(message)
       @logger.error(message)
     end
